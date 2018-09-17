cd37381bbe2d0b96326d9ad580c2d3002d0e8034
Sumit Sharma <sumit.sharma@aryavratinfotech.com>
2018-09-17 09:59:06 +0530

    Demo Project completed

--- src/Home.js
+++ src/Home.js
@@ -1,81 +1,98 @@
 import React, { Component } from "react";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 //import {Route,Link } from "react-router-dom";
 import "./Home.css";
 import Gallery from 'react-grid-gallery';
-import{ IMAGES} from './posters'
+import { IMAGES } from './posters'
+import ReactLoading from 'react-loading';
 import CharactersList from './charactersList'
 
 var { graphql, buildSchema } = require('graphql');
 
-var charArray=[]
+var charArray = []
 //
 export default class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-     nav:true,
-     
+      nav: true,
+      loader: false,
+      charcters: []
+
     };
   }
-componentWillMount(){
- 
-}
+  componentWillMount() {
 
-getData(index){
-  charArray =[]
- fetch("https://swapi.co/api/films/"+(index+1))
-  .then(function(response) {
-   return response.json();
-})
-.then(function(myJson) {
-  var data =myJson.characters
-  data.map((re,index)=>{
-    let a =JSON.stringify(re)
- if(a.length==32){
-   var b=a.slice(29,30)
-  fetch("https://swapi.co/api/people/"+b)
-  .then(function(response) {
-   return response.json();
- })
- .then(function(myJson) {
-  charArray.push(myJson)
-  console.log(charArray);
-  
- })
-}
-else{
-  var b=a.slice(29,31)
-  fetch("https://swapi.co/api/people/"+b)
-  .then(function(response) {
-   return response.json();
- })
- .then(function(myJson) {
-  charArray.push(myJson)
-  console.log(charArray);
-  
- })
-}
- 
+
   }
-)
-  
-});
 
-}
+  getData(index) {
+    this.state.charcters = []
+    const charArray = ((array) => {
+      this.state.charcters.push(array)
+      this.setState({ nav: false })
+      this.setState({ loader: false })
+    })
+    fetch("https://swapi.co/api/films/" + (index + 1))        //fetching films data list
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        var data = myJson.characters
+
+        data.map((re, index) => {
+          let a = JSON.stringify(re)
+          if (a.length == 32) {
+            var b = a.slice(29, 30)
+            fetch("https://swapi.co/api/people/" + b)            //fetching charcters data list
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (myJson) {
+                charArray(myJson)
+              })
+          }
+          else {
+            var b = a.slice(29, 31)
+            fetch("https://swapi.co/api/people/" + b)
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (myJson) {
+                charArray(myJson)
+
+              })
+          }
+
+        }
+        )
+
+      });
+
+  }
   render() {
 
     return (
       <div className="Login">
-        <div className="Header1" ><div className="Header"> {this.state.nav?<h1 style={{color:'white',alignSelf:'center'}}>🔥StarWar Series🔥</h1>:
-           <div><Button onClick={()=>this.setState({nav:true})}>{'<<'}</Button>
-           <h1 style={{color:'white',alignSelf:'center'}}>🔥Charters and Rolls🔥</h1></div>}
-               </div></div>
-      <div className="Body">
-      {/* <h3>Select any image for open more...</h3> */}
-     {this.state.nav?<Gallery images={IMAGES} onClickThumbnail={(index)=>{ this.getData(index)
-     }}  />:<CharactersList data ={charArray}/>}</div>
+        <div className="Header1" >
+          <div className="Header"> {this.state.nav ?
+            <h1 style={{ color: 'white', alignSelf: 'center' }}>🔥StarWar Series🔥</h1> :
+            <div>
+              <Button onClick={() => this.setState({ nav: true })}>{'<<'}</Button>
+              <h1 style={{ color: 'white', alignSelf: 'center' }}>🔥Charters and Rolls🔥</h1>
+            </div>}
+          </div>
+        </div>
+        <div className="Body">
+          {this.state.loader ? <div className="Header1" style={{ height: 'auto', paddingLeft: '45%' }}>
+            <ReactLoading type="spinningBubbles" color="#fff" />
+          </div> : ''}
+          {/* <h3>Select any image for open more...</h3> */}
+          {this.state.nav ? <Gallery images={IMAGES} onClickThumbnail={(index) => {
+            this.setState({ loader: true }), this.getData(index)
+          }} /> : <CharactersList data={this.state.charcters} />}
+        </div>
       </div>
     );
   }
--- src/charactersList.js
+++ src/charactersList.js
@@ -1,44 +1,67 @@
 
 import React, { Component } from "react";
-import {ListGroup, ListGroupItem,Grid, Row, Col,Button,Label} from "react-bootstrap";
-//import {Route,Link } from "react-router-dom";
-//import "./CharctersList.css";
-import Gallery from 'react-grid-gallery';
-import {people }from './temp'
-import Profile from './profile'
-//import IMAGES from './posters'
-var { graphql, buildSchema } = require('graphql');
-const characterArray =[{'name':'jadu'},{'name':'jadu'},{'name':'jadu'},{'name':'jadu'},{'name':'jadu'},{'name':'jadu'},{'name':'jadu'},
-{'name':'jadu'},{'name':'jadu'},{'name':'jadu'},
-{'name':'jadu'},{'name':'jadu'},{'name':'jadu'},{'name':'jadu'},
-{'name':'jadu'}]
+import { ListGroup, ListGroupItem, Grid, Row, Col, Button, Label } from "react-bootstrap";
+import Profile from './profile';
+import ReactLoading from 'react-loading';
 export default class CharactersList extends Component {
-    constructor(props) {
+    constructor(props) {                //constructor
         super(props);
-    
-        this.state = {
-         character:null,
-         change:false
-        };
-      }
 
-    render(){
-        console.log(this.props.data);
-        
-        return(
+        this.state = {
+            character: null,     //for character
+            change: false,      //for navigaion states
+            loader: false,     //for loader states
+            planet: ''
+        };
+    }
+    getData(url) {                          //fetching planet name
+        const charArray = ((name) => {
+
+            this.setState({ planet: name })
+            console.log(this.state.planet);
+
+            this.setState({ change: true })
+            this.setState({ loader: false })
+        })
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                charArray(myJson.name)
+
+
+            })
+
+    }
+
+    render() {
+
+
+        return (
             <div className="Login">
-           <Grid>
-  <Row className="show-grid">
-    <Col md={4} mdPush={4}><ListGroup>
-    <ListGroupItem><div className='Header1' style={{height:'auto'}}>List Of Characters</div></ListGroupItem>
-        {
-               this.props.data.map((data,index)=>(<ListGroupItem> <Button bsSize="large" active onClick={()=>{this.setState({character:data}),this.setState({change:true})}}>{data.name}</Button></ListGroupItem>))
-           }
-           </ListGroup></Col>
-        <Col md={8} mdPush={8}><Profile flag={this.state.change} character={this.state.character}/></Col></Row></Grid>
+                <Grid>
+                    <Row className="show-grid">
+                        <Col md={4} mdPush={4}><ListGroup>
+        <ListGroupItem><div className='Header1' style={{ height: 'auto' }}>List Of Characters</div></ListGroupItem>         {/*Character list*/}
+                            {
+                                this.props.data.map((data, index) => (<ListGroupItem> <Button bsSize="large" active onClick={() => {
+                                    this.setState({ character: data }),
+                                        this.getData(data.homeworld),
+                                        this.setState({ loader: true })
+                                }}>{data.name}</Button>
+                                </ListGroupItem>))
+                            }
+                        </ListGroup></Col>
+                        <Col md={8} mdPush={8}>{this.state.loader ? <div className="Header1" style={{ height: 'auto', paddingLeft: '45%' }}>
+                            <ReactLoading type="spinningBubbles" color="#fff" /></div> : ''}                                                                 {/*Character's profile*/}
+                            <Profile flag={this.state.change} character={this.state.character} homeworld={this.state.planet} />
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
-            
-             
+
+
         )
     }
 }
